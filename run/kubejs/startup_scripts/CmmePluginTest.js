3c05a57c000ba29520e07a1669e5359ac62c5f61
cristalpower2 <elisei12131@yahoo.com>
2025-08-26 12:13:30 +0300

    texture additions + Logo

--- run/kubejs/startup_scripts/CmmePluginTest.js
+++ run/kubejs/startup_scripts/CmmePluginTest.js
@@ -1,3 +1,12 @@
 GTCEuStartupEvents.registry('gtceu:material', event => {
-     GTMaterials.Tin.addFlags(CMMEMaterialFlags.GENERATE_QUADRUPLE_INGOT)
+     GTMaterials.Tin.addFlags(
+         CMMEMaterialFlags.GENERATE_DOUBLE_INGOT,
+         CMMEMaterialFlags.GENERATE_TRIPLE_INGOT,
+         CMMEMaterialFlags.GENERATE_QUADRUPLE_INGOT,
+         CMMEMaterialFlags.GENERATE_QUINTUPLE_INGOT,
+         CMMEMaterialFlags.GENERATE_TRIPLE_PLATE,
+         CMMEMaterialFlags.GENERATE_QUADRUPLE_PLATE,
+         CMMEMaterialFlags.GENERATE_QUINTUPLE_PLATE,
+         CMMEMaterialFlags.GENERATE_SINGULARITY
+     )
 })
